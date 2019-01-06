7f19f02603e323a60517d81ed2f110f8ca2851b9
Luca Santarella <luca.santarella@gmail.com>
2019-01-06 14:05:34 -0500

    Updated port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 const wampserver = require('wamp-server');
 const server = new wampserver({
-  port: 8080,
+  port: 9080,
   realms: ['com.lucasantarella.iot'], // array or string
 });
